12e5aa07db9e3dc626c9996ba9983294103e622d
Nvertigo <nvertigo67@gmail.com>
2014-11-08 15:40:45 +0100

    make popup windows smaller.

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_CHARACTERISTICS := tablet
 DEVICE_PACKAGE_OVERLAYS += device/samsung/chagallwifi/overlay
 
 # Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_CONFIG := xlarge hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
