CyanogenMod/android_device_samsung_picassowifi

picassowifi: sepolicy: Replace with manta's sepolicy
Change-Id: I9f7f744a05f5c6213b90d3e3bdb7f388d43c74da

BoardConfig.mk
@@ -126,22 +126,19 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_USES_SCALER := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS := \
-    device/samsung/picassowifi/sepolicy
+BOARD_SEPOLICY_DIRS += \
+	device/samsung/picassowifi/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-    file_contexts \
-    genfs_contexts \
-    adbd.te \
-    app.te \
-    device.te \
-    domain.te \
-    gpsd.te \
-    file.te \
-    mediaserver.te \
-    surfaceflinger.te \
-    samsung_media.te \
-    system.te
+BOARD_SEPOLICY_UNION += \
+	file_contexts \
+	device.te \
+	domain.te \
+	drmserver.te \
+	healthd.te \
+	gpsd.te \
+	file.te \
+	mediaserver.te \
+	system_server.te
 
 # SurfaceFlinger
 BOARD_USES_SYNC_MODE_FOR_MEDIA := true

exynos5420/android_device_samsung_chagallwifi

Conflicts:
	BoardConfig.mk

BoardConfig.mk
@@ -135,22 +135,20 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_USES_SCALER := true
 
 # SELinux
+<<<<<<< HEAD
 BOARD_SEPOLICY_DIRS := \
-    device/samsung/chagallwifi/sepolicy
+	device/samsung/chagallwifi/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-    file_contexts \
-    genfs_contexts \
-    adbd.te \
-    app.te \
-    device.te \
-    domain.te \
-    gpsd.te \
-    file.te \
-    mediaserver.te \
-    surfaceflinger.te \
-    samsung_media.te \
-    system.te
+BOARD_SEPOLICY_UNION += \
+	file_contexts \
+	device.te \
+	domain.te \
+	drmserver.te \
+	healthd.te \
+	gpsd.te \
+	file.te \
+	mediaserver.te \
+	system_server.te
 
 # SurfaceFlinger
 BOARD_USES_SYNC_MODE_FOR_MEDIA := true