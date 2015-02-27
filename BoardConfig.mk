7c024bad6a1032998bbb71ebc7bc2df020f4e71e
Nvertigo <nvertigo67@gmail.com>
2015-02-27 18:33:58 +0100

    opt in show percentage in powered off charging. See also: http://review.cyanogenmod.org/#/c/85588/3/healthd/Android.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,7 @@ TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 
 # Charging mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
 
