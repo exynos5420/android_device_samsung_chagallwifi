318b188afe1da17d55e9cf067393de4629c14724
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-01-02 15:32:15 +0530

    Disable dalvik swap and set filter to speed
    Change-Id: Id77d8bc295190b67028f0c227e0243f955c0544f

--- device.mk
+++ device.mk
@@ -213,6 +213,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.dex2oat-swap=false
+
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
