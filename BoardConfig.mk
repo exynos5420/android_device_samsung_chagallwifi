exynos5420/android_device_samsung_klimtlte

Switch to deathly defconfig
Change-Id: I6020283b887393f0e240dccb5916509b812e46be

BoardConfig.mk
@@ -63,7 +63,7 @@ COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogenmod_klimtlte_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_deathly_klimtlte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 

exynos5420/android_device_samsung_chagallwifi

BoardConfig.mk
@@ -62,7 +62,7 @@ COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogenmod_chagalllte_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_deathly_chagalllte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
 
 # Charging mode