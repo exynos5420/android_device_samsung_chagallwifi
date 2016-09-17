432664be21ac32950e6d214439686c2baff4f051
Sean Hoyt <seanhoyt963@gmail.com>
2016-09-16 21:45:05 -0500

    Disable livedisplay by default
    The mDNIE display modes look so much better and they do not work with
    livedisplay enabled
    
    Stop building advanced display as well because live display has the same
    feature because we do not support the negative option.
    
    Change-Id: I3c942163f1ac0a4f55c1495311418cddb2a424d2

--- device.mk
+++ device.mk
@@ -218,10 +218,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.universal5420
 
-# hardware/samsung/AdvancedDisplay (MDNIE)
-PRODUCT_PACKAGES += \
-    AdvancedDisplay
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
