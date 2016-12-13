0e62f7ea3ef1718b8fbbda478bd21c2aa0393a1c
Sean Hoyt <seanhoyt963@gmail.com>
2016-12-12 19:11:34 -0600

    Liquify
    Change-Id: I3894899431e3189949973d126ba4767cbae21e52

--- ld.mk
+++ ld.mk
@@ -1,5 +1,9 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/chagalllte/full_chagalllte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
+# Inherit from chagalllte device
+$(call inherit-product, device/samsung/chagalllte/device.mk)
 
 # Inherit some common LD stuff.
 $(call inherit-product, vendor/ld/config/common_full_phone.mk)
@@ -8,7 +12,7 @@ PRODUCT_NAME := ld_chagalllte
 PRODUCT_DEVICE := chagalllte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=SM-T805 \
+    PRODUCT_MODEL=SM-T705 \
     PRODUCT_NAME=chagalllte \
     PRODUCT_DEVICE=chagalllte \
     TARGET_DEVICE=chagalllte
