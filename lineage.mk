c9158771bceaebefde10775b53f5c2c8fc7d7bcb
Thomas Patry <thompatry@gmail.com>
2017-01-07 15:15:02 -0600

    chagalllte: Rebrand to LineageOS
    Change-Id: I8a716bf61c46da9719befc20dda2233e98457f45

--- lineage.mk
+++ lineage.mk
@@ -2,15 +2,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
+# Inherit from chagalllte device
 $(call inherit-product, device/samsung/chagalllte/device.mk)
 
-# Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+PRODUCT_BRAND := samsung
 
-# Inherit more AICP stuff.
-$(call inherit-product, vendor/aicp/configs/telephony.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := aicp_chagalllte
+# Inherit more LineageOS stuff.
+$(call inherit-product, vendor/cm/config/telephony.mk)
+
+PRODUCT_NAME := lineage_chagalllte
 PRODUCT_DEVICE := chagalllte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
