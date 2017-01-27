7862231a07c18c1e190c131e3218451b1bbec21a
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-27 23:33:06 +0100

    cmhw: Add MDNIE RGB tuning capabilities
    Change-Id: If3c2baf64cea2429bf34e54fd8164c1c2e32e94c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,9 @@ BOARD_SEPOLICY_DIRS += device/samsung/klimtlte/sepolicy
 # Fixes screen flicker
 TARGET_FORCE_SCREENSHOT_CPU_PATH := true
 
+# Cyanogen Hardware
+BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw
+
 # custom additions to updater-script
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagalllte/releasetools/ota_from_target_files
 
