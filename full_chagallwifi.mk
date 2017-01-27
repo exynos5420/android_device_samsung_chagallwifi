2beeaaa932854dd5da7613ec1d46c546bc0f2390
mosimchah <mosimchah@gmail.com>
2017-01-26 19:09:49 -0500

    init chagallwifi
    Change-Id: Ieeaf2fce3262430fdf8f9042673561449239a87b

--- full_chagallwifi.mk
+++ full_chagallwifi.mk
@@ -4,20 +4,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from chagallwifi device
 $(call inherit-product, device/samsung/chagallwifi/device.mk)
 
+# Discard inherited values and use our own instead.
 PRODUCT_NAME := full_chagallwifi
 PRODUCT_DEVICE := chagallwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SM-T800
-
-# Inherit some common LD stuff.
-$(call inherit-product, vendor/ld/config/common_full_tablet_wifionly.mk)
-
-PRODUCT_NAME := ld_chagallwifi
-PRODUCT_DEVICE := chagallwifi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=SM-T800 \
-    PRODUCT_NAME=chagallwifi \
-    PRODUCT_DEVICE=chagallwifi \
-    TARGET_DEVICE=chagallwifi
