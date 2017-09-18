68e71e78154116a713848c45fb1fa0fb03d9b08c
Darragh McGee <darraghmcgee20@gmail.com>
2017-09-18 21:34:04 +0200

    chagallwifi: update vendor path in lineage.mk
    Change-Id: I3faae4ba10d0365267ff97023cb13fcbb94cfd1b

--- lineage.mk
+++ lineage.mk
@@ -11,7 +11,7 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SM-T800
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_chagallwifi
 PRODUCT_DEVICE := chagallwifi
