173d6fcf8f80100a0ffbe58d0f74e114bd6c9bd0
Darragh McGee <darraghmcgee20@gmail.com>
2018-02-04 19:36:17 +0000

    chagallwifi: Another thing to change to lineagehw
    Change-Id: Id6a3cfcb86562612f4c19a218df43ec39eb7240b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
 TARGET_FORCE_SCREENSHOT_CPU_PATH := true
 
 # Cyanogen Hardware
-BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw
+BOARD_HARDWARE_CLASS := $(COMMON_PATH)/lineagehw
 
 # custom additions to updater-script
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagallwifi/releasetools/ota_from_target_files
