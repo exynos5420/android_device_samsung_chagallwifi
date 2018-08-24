bd8d4eeb7591bcf288643913583ef9e674774fc7
Thomas Patry <thompatry@gmail.com>
2018-08-24 11:15:26 -0400

    chagallwifi: Lets get shimmy with the GPS
    Change-Id: I06ddaf765bc363e86616265755898e3e00adffa9

--- libshim/libshim.cpp
+++ libshim/libshim.cpp
@@ -2,3 +2,6 @@
 // android::SensorManager::getSensorList(android::Sensor const* const**) const
 extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE();
 extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(){}
+
+extern "C" void _ZN7android13SensorManagerD1Ev();
+extern "C" void _ZN7android13SensorManagerD1Ev(){}
