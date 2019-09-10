dcebc0a8d127fe346428c3478644fa4bcf1b495c
Jan Altensen <info@stricted.net>
2019-09-10 11:52:20 +0200

    chagallwifi: gpsd: replace SSLv3_client_method with SSLv23_method
    Change-Id: I7aa2dc778d01bc91f2e4572143ccb37ec5d17a75

--- extract-files.sh
+++ extract-files.sh
@@ -71,4 +71,7 @@ BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 # Vendor separation
 sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libfloatingfeature.so
 
+# replace SSLv3_client_method with SSLv23_method
+sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/vendor/bin/gpsd
+
 "${MY_DIR}/setup-makefiles.sh"
