c6bec5b6f4cd13e37e977fe5e3b38b3220a6237c
Elim Garak <elim.garak.dev@gmail.com>
2021-02-14 08:45:11 -0500

    chagallwifi: Set build fingerprint from latest stock
    Add build fingerprint from stock T800XXU1CRJ1.
    This allows passing SafetyNet profile check.
    
    Change-Id: I34fd642cd924cee73b89615994da5c6df5120527

--- lineage_chagallwifi.mk
+++ lineage_chagallwifi.mk
@@ -34,4 +34,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T800 \
     PRODUCT_NAME=chagallwifi \
     PRODUCT_DEVICE=chagallwifi \
+    PRIVATE_BUILD_DESC="samsung/chagallwifixx/chagallwifi:6.0.1/MMB29K/T800XXU1CRJ1:user/release-keys" \
     TARGET_DEVICE=chagallwifi
+
+BUILD_FINGERPRINT := samsung/chagallwifixx/chagallwifi:6.0.1/MMB29K/T800XXU1CRJ1:user/release-keys
+
