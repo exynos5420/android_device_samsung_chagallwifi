abd905ee979cd207b568e08a6fa8117df62b0dfa
Elim Garak <elim.garak.dev@gmail.com>
2021-02-20 15:27:46 -0500

    chagallwifi: system_prop.mk: cleaning unnecessary and duplicate properties
    Change-Id: I3a493c3629b781afdd36911ad9ebf3f7e4fa3ba3

--- system_prop.mk
+++ system_prop.mk
@@ -2,30 +2,10 @@
 # system.prop for chagallwifi
 #
 
-# UI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=320
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.radio.noril=yes
 
-# Randomly from stock
+# UI
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.gprsclass=10 \
-    ro.ril.hsxpa=1 \
-    ro.sec.fle.encryption=true \
-    ro.secwvk=220
-
-# SM-T800 Hardware Info
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.chipset=Exynos_5_Octa_5420 \
-    ro.device.cpu=Octa-core,1900MHz,ARM_Cortex_A15_&_ARM_Cortex_A7 \
-    ro.device.gpu=ARM_Mali-T628_MP6 \
-    ro.device.rear_cam=8MP,1920x1080_(1080pHD)_(30fps) \
-    ro.device.front_cam=2.1MP \
-    ro.device.screen_res=2560x1600pixels_(288ppi_pixel_density)
-
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sdcardfs.enable=false
+    ro.sf.lcd_density=320
